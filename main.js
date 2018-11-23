45813dda19bcd96a680564a81f3f01e69a583f32
admin <admin@onlimages.com>
2018-11-23 22:28:45 +0530

    adding script for contact form

---
+++ main.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/5.5.9/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAEaVw_zTYkVnDUmoklQh9dQCLxstvcePU",
+    authDomain: "landing-contact-form.firebaseapp.com",
+    databaseURL: "https://landing-contact-form.firebaseio.com",
+    projectId: "landing-contact-form",
+    storageBucket: "landing-contact-form.appspot.com",
+    messagingSenderId: "608318984006"
+  };
+  firebase.initializeApp(config);
+</script>
