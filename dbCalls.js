d4a949126bc8e94f1fd2c839a5e7b52b30355424
admin <admin@onlimages.com>
2018-11-24 15:58:17 +0530

    lateloading working

---
+++ dbCalls.js
@@ -0,0 +1,43 @@
+var successHTML = "";
+
+var showSuccessHTML = showSuccessHTML;
+var saveData = saveData;
+
+function saveData () {
+    // START: FIREBASE CONFIGURATION
+    var db_fireObj;
+
+    firebase.initializeApp({
+      apiKey: "AIzaSyAEaVw_zTYkVnDUmoklQh9dQCLxstvcePU",
+      authDomain: "landing-contact-form.firebaseapp.com",
+      databaseURL: "https://landing-contact-form.firebaseio.com",
+      projectId: "landing-contact-form",
+      storageBucket: "landing-contact-form.appspot.com",
+      messagingSenderId: "608318984006"
+    });
+
+    db_fireObj = firebase.firestore();  
+    db_fireObj.settings({
+      timestampsInSnapshots: true
+    });
+    // END: FIREBASE CONFIGURATION
+
+    db_fireObj.collection('contacts').add({
+      Name: elemName.value,
+      Email: elemEmail.value
+    }).then(function(docRef) {
+// TO DO: MAKE BROWSER NOT REMEMBER THE LAST ENTERED TEXTS
+// TO DO: BACK-END TO RESTRICT ONLY 10 REQUESTS PER DAY FROM A IP
+        console.log("Document written with ID: ", docRef.id, ' @', Date.now());
+// TO DO: REMOVE resetForm & hideLoading IF RELOADING PAGE
+        resetForm();
+        hideLoading();
+        showSuccessHTML();
+    }).catch( function (error) {
+        console.error("Error adding document: ", error);
+    });
+} // FN SAVE-DATA
+  
+function showSuccessHTML () {
+    elemBody.innerHTML = successHTML;
+} // FN SHOW-SUCCESS-HTML
--- main.js
+++ main.js
@@ -1,24 +1,17 @@
 
-// <script src="https://www.gstatic.com/firebasejs/5.5.9/firebase.js"></script>
-// <script>
-//   // Initialize Firebase
-//   var config = {
-//     apiKey: "AIzaSyAEaVw_zTYkVnDUmoklQh9dQCLxstvcePU",
-//     authDomain: "landing-contact-form.firebaseapp.com",
-//     databaseURL: "https://landing-contact-form.firebaseio.com",
-//     projectId: "landing-contact-form",
-//     storageBucket: "landing-contact-form.appspot.com",
-//     messagingSenderId: "608318984006"
-//   };
-//   firebase.initializeApp(config);
-// </script>
-var saveData = saveData;
 var validateForm = validateForm;
 var resetForm = resetForm;
+var showLoading = showLoading;
+var hideLoading = hideLoading;
+var showHideMsg = showHideMsg;
+var loadScript = loadScript;
+var getAdScript = getAdScript;
+var loadFirebase = loadFirebase;
 
 var elemName = document.getElementById('inpName');
 var elemEmail = document.getElementById('inpEmail');
 var elemErrorMsg = document.getElementById('spanErrorMsg');
+var elemBody = document.getElementsByTagName('body');
 
 var filterEmail = /^([a-zA-Z])([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
@@ -46,8 +39,9 @@ function validateForm () {
     elemErrorMsg.innerHTML = "Please provide a valid Email Address";
     elemEmail.focus();
   } else {
+    showLoading();
     elemErrorMsg.innerHTML = "";
-    saveData();
+    getAdScript();
     return;
   }
 
@@ -75,40 +69,13 @@ function showHideMsg () {
   }
 } // FN SHOW-HIDE-MSG
 
-function saveData () {
-  showLoading();
-  loadScript("https://www.gstatic.com/firebasejs/5.5.9/firebase.js", sendValues);
-
+function getAdScript () {
+  loadScript("./dbCalls.js", loadFirebase);
 } // FN SAVE-DATA
 
-function sendValues () {
-  firebase.initializeApp({
-    apiKey: "AIzaSyAEaVw_zTYkVnDUmoklQh9dQCLxstvcePU",
-    authDomain: "landing-contact-form.firebaseapp.com",
-    databaseURL: "https://landing-contact-form.firebaseio.com",
-    projectId: "landing-contact-form",
-    storageBucket: "landing-contact-form.appspot.com",
-    messagingSenderId: "608318984006"
-  });
-  var db_fireObj = firebase.firestore();
-
-  db_fireObj.settings({
-    timestampsInSnapshots: true
-  });
-
-  db_fireObj.collection('contacts').add({
-    Name: elemName.value,
-    Email: elemEmail.value
-    }).then(function(docRef) {
-    // TO DO: MAKE BROWSER NOT REMEMBER THE LAST ENTERED TEXTS
-    // TO DO: BACK-END TO RESTRICT ONLY 10 REQUESTS PER DAY FROM A IP
-        console.log("Document written with ID: ", docRef.id, ' @', Date.now());
-        resetForm();
-        hideLoading();
-    }).catch( function (error) {
-        console.error("Error adding document: ", error);
-    });
-} // FN SEND-VALUES
+function loadFirebase () {
+  loadScript("https://www.gstatic.com/firebasejs/5.5.9/firebase.js", saveData);
+}
 
 function resetForm () {
   elemName.value = "";
