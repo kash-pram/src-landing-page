f99600abe7e4dddf4cc7e6ecf08d01c7d1552dd0
admin <admin@onlimages.com>
2018-11-24 09:11:07 +0530

    fixed email regex

--- main.js
+++ main.js
@@ -17,12 +17,15 @@
 function checkEmail() {
 
   var email = document.getElementById('inpEmail');
+  var emailError = document.getElementById('spanEmailError');
   // var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  var filter = /^([a-zA-Z])?([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  var filter = /^([a-zA-Z])([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
   if (!filter.test(email.value)) {
-    alert('Please provide a valid email address');
+    emailError.innerHTML = "Please provide a valid email address";
     email.focus;
     return false;
+  } else {
+    emailError.innerHTML = "";
   }
 }
